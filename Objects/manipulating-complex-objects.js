9ee3e9f55cbf9ac80d626bc9373515de72222751
Houdini Papi <ochiengcliff.co@gmail.com>
2023-03-25 10:56:04 +0300

    manipulating complex objects

---
+++ Objects/manipulating-complex-objects.js
@@ -0,0 +1,23 @@
+const myMusic = [
+  {
+    "artist": "Papi Chulo",
+    "title": "The Coder",
+    "release_year": 2023,
+    "formats": [
+      "Mp3",
+      "Mp4",
+      "4K"
+      ]
+  },
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
