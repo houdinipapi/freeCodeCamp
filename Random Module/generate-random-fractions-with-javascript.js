0243f5866e16067511d9a16db789fb27ef923e4b
Houdini Papi <ochiengcliff.co@gmail.com>
2023-03-25 22:28:30 +0300

    Generate Random Fractions with JavaScript

---
+++ Random Module/generate-random-fractions-with-javascript.js
@@ -0,0 +1,9 @@
+/**
+ * Math.random() function generates a random decimal number btwn 0 (inclusive) and 1 (exclusive).
+ * the function can return a 0 but never a 1
+ */
+
+function randomFraction() {
+
+  return Math.random();
+}
