cfc504c08e954b48abb49f8c17d7a113361b0491
Houdini Papi <ochiengcliff.co@gmail.com>
2023-03-26 23:55:35 +0300

    Use Destructuring Assignment to Extract Values from Objects

---
+++ ES6/Destructuring Assignment/use-destructuring-assignment-to-extract-values-from-objects.js
@@ -0,0 +1,24 @@
+/**
+ * Replace the two assignments with an equivalent destructuring assignment. It should still assign the variables today and tomorrow the values of today and tomorrow from the HIGH_TEMPERATURES object.
+ */
+
+// const HIGH_TEMPERATURES = {
+//   yesterday: 75,
+//   today: 77,
+//   tomorrow: 80
+// };
+
+// // Only change code below this line
+
+// const today = HIGH_TEMPERATURES.today;
+// const tomorrow = HIGH_TEMPERATURES.tomorrow;
+
+// // Only change code above this line
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+
+const {today, tomorrow} = HIGH_TEMPERATURES;
