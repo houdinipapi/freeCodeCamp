c3cb56c21fd52e13b33e9955c808a35e25504809
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-03 21:52:03 +0300

    check for mixed grouping of characters

---
+++ JavaScript/Regular Expressions (regex)/Test Method/check-for-mixed-grouping-of-characters.js
@@ -0,0 +1,6 @@
+//check for mixed grouping of characters
+
+let myString = "Eleanor Roosevelt";
+let myRegex = /(Franklin|Eleanor) (([A-Z]\.?|[A-Z][a-z]+) )?Roosevelt/;
+let result = myRegex.test(myString);
+// After passing the challenge experiment with myString and see how the grouping works
