3214bbdd1ce3007747c0c63392eb6da8b1c112c9
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-03 22:03:34 +0300

    use capture groups to search and replace

---
+++ JavaScript/Regular Expressions (regex)/Test Method/use-capture-groups-to-search-and-replace.js
@@ -0,0 +1,6 @@
+// Use capture groups to search and replace.
+
+let str = "one two three";
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/;
+let replaceText = "$3 $2 $1";
+let result = str.replace(fixRegex, replaceText);
