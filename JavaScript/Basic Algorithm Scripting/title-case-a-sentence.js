9a878147bdb86b00c7dbccf63ef7019bd93c23fe
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-05 19:03:43 +0300

    Title Case a Sentence

---
+++ JavaScript/Basic Algorithm Scripting/title-case-a-sentence.js
@@ -0,0 +1,10 @@
+/**
+ * Return the provided string with the first letter of each word capitalized.
+ * Make sure the rest of the word is in lowercase.
+ */
+
+function titleCase(str) {
+    return str
+    .toLowerCase()
+    .replace(/(^|\s)\S/g, L => L.toUpperCase());
+}
