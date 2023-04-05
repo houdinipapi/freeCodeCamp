1d17dc4b8b1089dee958e6c1a4dfab5a69555732
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-05 18:58:06 +0300

    Boo Who

---
+++ JavaScript/Basic Algorithm Scripting/boo-who.js
@@ -0,0 +1,17 @@
+/**
+ * check if a value is classified as a boolean primitive.
+ * return true or false;
+ * Boolean primitives are true and false.
+ */
+
+function booWho(bool)
+{
+    if (bool === true | bool === false)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
