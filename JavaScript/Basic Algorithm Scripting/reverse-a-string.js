44ffb2d0c3df8604727d347dc97c20046532811e
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-05 19:02:59 +0300

    Reverse a String

---
+++ JavaScript/Basic Algorithm Scripting/reverse-a-string.js
@@ -0,0 +1,14 @@
+/**
+ * reverse the provided string
+ * return the reversed string
+ */
+
+function reverseString(str) {
+    let reversedStr = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedStr += str[i];
+    }
+    return reversedStr;
+}
+
+reverseString("hello");
