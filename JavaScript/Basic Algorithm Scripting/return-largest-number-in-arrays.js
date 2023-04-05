a11996f7a514348eab79fcc7dca4eb116371ba19
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-05 19:02:37 +0300

    Return Largest Numbers in Arrays

---
+++ JavaScript/Basic Algorithm Scripting/return-largest-number-in-arrays.js
@@ -0,0 +1,24 @@
+/**
+ * Return an array consisting of the largest number from each provided sub-array.
+ * You can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+ */
+
+function largestOfFour(arr)
+{
+    const results = [];
+
+    for (let i = 0; i < arr.length; i++)
+    {
+        let largestNumber = arr[i][0];
+        for (let j = 1; j < arr[i].length; j++)
+        {
+            if (arr[i][j] > largestNumber)
+            {
+                largestNumber = arr[i][j];
+            }
+        }
+        results[i] = largestNumber;
+    }
+
+    return results;
+}
