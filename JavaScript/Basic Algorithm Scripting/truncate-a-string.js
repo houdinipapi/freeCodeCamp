f7ccfad5bad10a0ea615f575a236f3e55393b3f3
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-05 19:04:04 +0300

    Truncate a String

---
+++ JavaScript/Basic Algorithm Scripting/truncate-a-string.js
@@ -0,0 +1,16 @@
+/**
+ * truncate a string(first argument) if it is longer than the given maximum string length(second argument).
+ * Return the truncated string with a ... ending.
+ */
+
+function truncateString(str, num)
+{
+    if (str.length > num)
+    {
+        return str.slice(0, num) + "...";
+    }
+    else
+    {
+        return str;
+    }
+}
