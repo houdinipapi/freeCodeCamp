c2a72ed132753e564f8048fc4a7503c0902535ca
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-06 18:57:36 +0300

    change the prototype to a new object

---
+++ JavaScript/Object Oriented Programming/change-the-prototype-to-a-new-object.js
@@ -0,0 +1,18 @@
+/**
+ * Add the property numLegs and two methods eat() and describe() to the prototype of Dog by setting the prototype to a new object.
+ */
+
+function Dog(name)
+{
+    this.name = name;
+}
+
+Dog.prototype = {
+    numLegs: 4,
+    eat: function() {
+        console.log("nom nom nom");
+    },
+    describe: function() {
+        console.log("My name is " + this.name);
+    }
+};
