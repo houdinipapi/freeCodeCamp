d7f06781efc0c8b90afab2d6f54edf9fc9a4200e
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-06 19:28:27 +0300

    use a constructor to create objects

---
+++ JavaScript/Object Oriented Programming/use-a-constructor-to-create-objects.js
@@ -0,0 +1,13 @@
+/**
+ * Use the Dog constructor to create a new instance of Dog, assigning it to a variable hound.
+ * hound should be created using the Dog constructor.
+ * Your code should use the new operator to create an instance of Dog.
+ */
+
+function Dog() {
+    this.name = "Spot";
+    this.color = "brown";
+    this.numLegs = 4;
+}
+
+let hound = new Dog();
