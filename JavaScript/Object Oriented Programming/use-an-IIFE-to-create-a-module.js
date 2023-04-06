21243505f4911064a7e7fa731c5b81d129abc300
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-06 19:29:38 +0300

    use an IIFE to create a module

---
+++ JavaScript/Object Oriented Programming/use-an-IIFE-to-create-a-module.js
@@ -0,0 +1,38 @@
+/**
+ * Create a module named funModule to wrap the two mixins isCuteMixin and singMixin.
+ * funModule should return an object.
+ * 
+ * **TESTS**
+ * funModule should be defined and return an object.
+ * funModule.isCuteMixin should access a function.
+ * funModule.singMixin should access a function.
+ */
+
+let isCuteMixin = function(obj) {
+    obj.isCute = function() {
+        return true;
+    };
+};
+
+let singMixin = function(obj) {
+    obj.sing = function() {
+        console.log("Singing to an awesome tune");
+    };
+};
+
+// Create a module
+let funModule = (function () {
+    return {
+        isCuteMixin: function(obj) {
+            obj.isCute = function() {
+                return true;
+            };
+        },
+
+        singMixin: function(obj) {
+            obj.sing = function() {
+                console.log("Singing to an awesome tune");
+            };
+        }
+    }
+})();
