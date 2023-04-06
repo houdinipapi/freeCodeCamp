405998583d80d3c4c24be961da6047529a729392
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-06 18:58:16 +0300

    create a basic JavaScript object

---
+++ JavaScript/Object Oriented Programming/create-a-basic-javascript-object.js
@@ -0,0 +1,9 @@
+/**
+ * create a dog object with name and numLegs properties.
+ * set the properties to a string and a number respectively.
+ */
+
+let dog = {
+    name: "Huncho",
+    numLegs: 4
+};
