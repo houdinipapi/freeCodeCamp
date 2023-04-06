0706107c4d5bf40754daa8b60b7483947910bd8d
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-06 19:31:06 +0300

    use dot notation to access the properties of an object

---
+++ JavaScript/Object Oriented Programming/use-dot-notation-to-access-the-properties-of-an-object.js
@@ -0,0 +1,12 @@
+/**
+ * print both properties of the dog object to your console
+ */
+
+let user = {
+    name: "Papi",
+    age: 25
+};
+
+// To print
+console.log(user.name);
+console.log(user.age);
