07ac8308864add6301c295454de0444e5b3b33b5
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-08 02:07:32 +0300

    functional programming

---
+++ JavaScript/Functional Programming/combine-an-array-into-a-string-using-the-join-method.js
@@ -0,0 +1,10 @@
+// Use the join method (among others) inside the sentensify function to make a sentence from the words in the string str.
+// The function should return a string. For example, I-like-Star-Wars would be converted to I like Star Wars.
+// Do not use the replace word.
+
+function sentensify(str) {
+
+    return str.split(/\W/).join(" ");
+}
+
+sentensify("May-the-force-be-with-you");
