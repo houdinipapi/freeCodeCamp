7efe59a945bfd59c8141a7ab82720e5854be2f43
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-07 23:52:53 +0300

    Functional Programming

---
+++ JavaScript/Functional Programming/pass-arguments-to-avoid-external-dependence-in-a-function.js
@@ -0,0 +1,15 @@
+// Let's update the incrementer function to clearly declare its dependencies.
+// Write the incrementer function so it takes an argument, and then returns a result after increasing the value by one.
+// TESTS
+// Your function incrementer should not change the value of fixedValue (which is 4).
+// Your incrementer function should take an argument.
+// Your incrementer function should return a value that is one larger than the fixedValue value.
+
+//The global variable
+let fixedValue = 4;
+
+function incrementer(fixedValue) {
+
+    return fixedValue + 1;
+
+}
