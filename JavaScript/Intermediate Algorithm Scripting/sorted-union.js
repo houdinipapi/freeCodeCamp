0cf03cde8ddba31ba413f6b78d81b6ed046a9ac1
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-12 20:58:04 +0300

    Sorted Union

---
+++ JavaScript/Intermediate Algorithm Scripting/sorted-union.js
@@ -0,0 +1,16 @@
+// Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+// In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+// The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+
+function uniteUnique(...arrs) {
+    let result = [];
+
+    for (let arr of arrs) {
+        for (let val of arr) {
+            if (!result.includes(val)) {
+                result.push(val);
+            }
+        }
+    }
+    return result;
+}
