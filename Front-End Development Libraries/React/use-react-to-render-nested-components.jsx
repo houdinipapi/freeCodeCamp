9b75667c4454896887fcb8a76cbea4d523c47776
Houdini Papi <ochiengcliff.co@gmail.com>
2023-04-18 17:04:36 +0300

    Use React to Render Nested Components

---
+++ Front-End Development Libraries/React/use-react-to-render-nested-components.jsx
@@ -0,0 +1,45 @@
+// The TypesOfFood component should return a single div element.
+// The TypesOfFood component should return the Fruits component.
+// The Fruits component should return the TypesOfFruit component.
+// The TypesOfFruit component should return the h2 and ul elements.
+
+const TypesOfFruit = () => {
+    return (
+        <div>
+            <h2>Fruits:</h2>
+                <ul>
+                    <li>Apples</li>
+                    <li>Blueberries</li>
+                    <li>Strawberries</li>
+                    <li>Bananas</li>
+                </ul>
+        </div>
+    );
+};
+
+const Fruits = () => {
+    return (
+        <div>
+            { /* Change the code below this line */ }
+            <TypesOfFruit />
+            { /* Change the code above this line */ }
+        </div>
+    );
+};
+
+class TypesOfFood extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Types of Food:</h1>
+                { /* Change code below this line */ }
+                <Fruits />
+                { /* Change code above this line */ }
+            </div>
+        );
+    }
+};
